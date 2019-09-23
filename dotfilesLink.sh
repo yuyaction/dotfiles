05cd4c45429df03ef4ec88eb70da866284d2f4ce
yuyaction <yuyaction@gmail.com>
2019-09-24 02:25:30 +0900

    colorに対応

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+if [ ! -d ~/.vim/colors ]; then
+    mkdir ~/.vim/colors
+    cp ./molokai.vim ~/.vim/colors
+fi
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
