6b4fb144304d4fd63752aec73e7f55f113b40854
yuyaction <yuyaction@gmail.com>
2019-11-14 18:59:44 +0900

    shファイルの書き換え

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 if [ ! -d ~/.vim ]; then
     mkdir ~/.vim
 fi
+if [ ! -d ~/.fonts ]; then
+    mkdir ~/.fonts
+fi
+cp ~/dotfiles/MyricaM.TTC ~/.fonts
 if [ ! -d ~/.vim/colors ]; then
     mkdir ~/.vim/colors
     cp ./molokai.vim ~/.vim/colors
 fi
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+cp  ~/dotfiles/.vimrc ~/
 
